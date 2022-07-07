c0dfeb474a5d5c1da7baeb9dccfff11757a01125
Edwin Boon <edwin.boon@eo.nl>
2022-07-07 12:16:07 +0200

    Implemented a simple tip calculator

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# code goes here
-print("Tip calculator")
+print("Welcome to the tip calculator")
+# ask user inputs
+bill = float(input("What was the total bill? €"))
+percentage_tip = int(input("What percentage tip would you like to give? "))
+number_of_people = int(input("How many people are spltting this bill? "))
+
+# calculate price per person
+total_bill = (bill / 100) * (100 + percentage_tip)
+bill_per_person = total_bill / number_of_people
+final_amount = "{:.2f}".format(bill_per_person, 2)
+
+# print result to the user
+print(f"Each person should pay: €{final_amount}")
